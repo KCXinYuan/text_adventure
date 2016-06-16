8900168356c95449beedc0aba4b959a8aafc10bd
Kevin Chuang <kcxinkun@gmail.com>
2016-06-16 12:38:05 -0700

    removed gulp cleaning

--- kevinChuang/gulpfile.js
+++ kevinChuang/gulpfile.js
@@ -2,10 +2,10 @@ const gulp = require('gulp');
 const webpack = require('webpack-stream');
 const clean = require('gulp-clean');
 
-gulp.task('clean', ()=> {
-  return gulp.src('build/**/**/**/*',{read:false})
-  .pipe(clean());
-});
+// gulp.task('clean', ()=> {
+//   return gulp.src('build/**/**/**/*',{read:false})
+//   .pipe(clean());
+// });
 
 gulp.task('copy', ()=> {
   gulp.src('./app/**/**/**/*')
@@ -22,6 +22,6 @@ gulp.task('bundle', ()=> {
   .pipe(gulp.dest('build/'));
 });
 
-gulp.task('build', ['clean', 'copy', 'bundle']);
+gulp.task('build', ['copy', 'bundle']);
 gulp.task('default', ['build']);
 gulp.watch(['app/*'],['build']);
